99f3cfb516215e9d73c84ec0e04a49bea0f751b4
Samet YOKTAN <yoktan81@gmail.com>
2025-03-18 13:15:56 +0300

    Project04 için yeni proje eklendi

---
+++ Project04/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+//Girilen yazılı ortalamasını hesaplayarak ortalama 50′ den küçükse Kaldı, değilse Geçti yazdıran C++ kodları
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sinav1, sinav2;
+	double ortalama;
+	cout << "1.Sayıyı Giriniz: ";
+	cin >> sinav1;
+	cout << "2.Sayıyı Giriniz: ";
+	cin >> sinav2;
+	ortalama = (sinav1 + sinav2) / 2;
+	if (ortalama >= 50)
+		cout << "Geçti: "<<ortalama;
+	else
+		cout << "Kaldı: "<<ortalama;
+}
