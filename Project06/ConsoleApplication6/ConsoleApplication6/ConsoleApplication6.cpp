ac44be1abb5e2581a5e77ae743756687b69d5896
Samet YOKTAN <yoktan81@gmail.com>
2025-03-18 13:19:20 +0300

    Project06 için yeni proje eklendi

---
+++ Project06/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+//C++ Girilen sayının 3′ e ve 5′ e tam bölünüp bülünmediğini gösteren C++ kodları
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayi;
+	cout << "Bir Sayı Giriniz: ";
+	cin >> sayi;
+	if (sayi % 3 == 0 && sayi % 5 == 0)
+		cout << "Sayı 3'e Ve 5'e Tam Bölünüyor";
+	else
+		cout << "Sayı 3'e Ve 5'e Tam Bölünmüyor";
+}
